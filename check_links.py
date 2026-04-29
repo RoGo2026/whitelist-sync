4f2f029c30b6209dd00fd6f4f3c4f5b6d3dd1317
RoGo2026 <romanbor555@gmail.com>
2026-04-29 17:19:18 +0300

    Update check_links.py

--- check_links.py
+++ check_links.py
@@ -8,8 +8,8 @@ import base64
 INPUT = "mobile-whitelist-1.txt"
 OUTPUT = "working_whitelist.txt"
 MAX_WORKERS = 20
-TEST_TIMEOUT = 2
-MAX_LATENCY_MS = 1000
+TEST_TIMEOUT = 3
+MAX_LATENCY_MS = 2000
 # ─────────────────────────────
 
 def parse_link(link):
